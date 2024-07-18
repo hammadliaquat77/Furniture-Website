ff22ba9adfb2440d6eaf99b4c100468af3accf45
Hammad Liaquat <154601362+hammadprogrammer77@users.noreply.github.com>
2024-07-18 11:25:20 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// let menu =document.querySelector('#menu-icon');
+let menu =document.getElementById('menu-icon');
+
+let navbar =document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+};
+
